b6ba8e17c1b4f4c3db2845ecf2da9dbb259c8658
qnsh <qinnianshun@gmail.com>
2024-02-27 16:14:44 +0800

    change Dropdown

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def ollama_chat(message, history,model_name):
 
 with gr.Blocks(title="Ollama Chat", fill_height=True) as demo:
     gr.Markdown("""# Ollama Webui""")
-    model_list = gr.Dropdown(model_names, value="", label="模型", info="选择聊天模型")
+    model_list = gr.Dropdown(model_names, value="", allow_custom_value=True, label="模型", info="选择聊天模型")
     text_box = gr.Textbox(scale=4,render=False)
     gr.ChatInterface(fn=ollama_chat,textbox=text_box,additional_inputs=model_list,submit_btn="提交",retry_btn="🔄 重试",undo_btn="↩️ 撤消",clear_btn="🗑️ 清除")
 
