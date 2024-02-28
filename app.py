b915136db38ceb9129a00b1579ba5c6d4eb60f33
qnsh <qinnianshun@gmail.com>
2024-02-28 09:32:59 +0800

    add chat history

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 import gradio as gr
 import ollama
 
+
 model_list = ollama.list()
 model_names = [model['model'] for model in model_list['models']]
-def ollama_chat(message, history,model_name):
-    stream = ollama.chat(
-        model= model_name,
-        messages=[
-            {
+def ollama_chat(message, history,model_name,history_flag):
+    messages = []
+    message = {
+        'role': 'user', 
+        'content': message
+    }
+    if history_flag and len(history)>0:
+        for elment in history:
+            history_message = {
                 'role': 'user', 
-                'content': message
+                'content': elment[0]
             }
-        ],
+            print(history_message) 
+            messages.append(history_message)   
+    messages.append(message)
+    stream = ollama.chat(
+        model = model_name,
+        messages = messages,
+        
         stream=True
     )
     partial_message = ""
@@ -23,8 +34,10 @@ def ollama_chat(message, history,model_name):
 with gr.Blocks(title="Ollama Chat", fill_height=True) as demo:
     gr.Markdown("""# Ollama Webui""")
     model_info = gr.Dropdown(model_names, value="", allow_custom_value=True, label="模型", info="选择聊天模型")
+    history_flag = gr.Checkbox(label="启用上下文")
     text_box = gr.Textbox(scale=4,render=False)
-    gr.ChatInterface(fn=ollama_chat,textbox=text_box,additional_inputs=model_info,submit_btn="提交",retry_btn="🔄 重试",undo_btn="↩️ 撤消",clear_btn="🗑️ 清除")
+    gr.ChatInterface(fn=ollama_chat,textbox=text_box,additional_inputs=[model_info,history_flag],submit_btn="提交",retry_btn="🔄 重试",undo_btn="↩️ 撤消",clear_btn="🗑️ 清除")
+    
 
 if __name__ == "__main__":
     demo.launch()
