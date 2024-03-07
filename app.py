a4df2a51d5358103bdb7867d9ab3be8468931a4d
qnsh <qinnianshun@gmail.com>
2024-03-07 13:31:23 +0800

    add llava

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import gradio as gr
 import ollama
 import json
+import base64
+import copy
 
 model_list = ollama.list()
 model_names = [model['model'] for model in model_list['models']]
 PROMPT_LIST = []
+VL_CHAT_LIST = []
 # 解析 prompt
 with open("prompt.json", "r", encoding="utf-8") as f:
     PROMPT_DICT = json.load(f)
     for key in PROMPT_DICT:
         PROMPT_LIST.append(key)
-# PROMPT_SELECT = ["翻译助手","周报助手"]
-# PROMPT_LIST = {
-#     "翻译助手":"你是一个好用的翻译助手。请将我的中文翻译成英文，将非中文的翻译成中文。我发给你所有的话都是需要翻译的内容，你只需要回答翻译结果。翻译结果请符合中文的语言习惯。",
-#     "周报助手":"请帮我把以下的工作内容填充为一篇完整的周报，用 markdown 格式以分点叙述的形式输出："
-# }
+# 初始化函数
+def init():
+    VL_CHAT_LIST.clear()
+# 是否包含中文
+def contains_chinese(string):
+    for char in string:
+        if '\u4e00' <= char <= '\u9fa5':
+            return True
+    return False
 def ollama_chat(message, history,model_name,history_flag):
     messages = []
     chat_message = {
@@ -67,7 +74,127 @@ def ollama_prompt(message, history,model_name,prompt_info):
         if len(chunk['message']['content']) != 0:
             partial_message = partial_message + chunk['message']['content']
             yield partial_message
-
+# 图片上传
+def vl_image_upload(image_path,chat_history):
+    messsage = {
+        "type":"image",
+        "content":image_path
+    }
+    chat_history.append(((image_path,),None))
+    VL_CHAT_LIST.append(messsage)
+    return None,chat_history
+# 提交问题
+def vl_submit_message(message,chat_history):
+    messsage = {
+        "type":"user",
+        "content":message
+    }
+    chat_history.append((message,None))
+    VL_CHAT_LIST.append(messsage)
+    return "",chat_history
+# 重试
+def vl_retry(chat_history):
+    if len(VL_CHAT_LIST)>1:
+        if VL_CHAT_LIST[len(VL_CHAT_LIST)-1]['type'] == "assistant":
+            VL_CHAT_LIST.pop()
+            chat_history.pop()
+    return chat_history
+# 撤销
+def vl_undo(chat_history):
+    message = ""
+    chat_list = copy.deepcopy(VL_CHAT_LIST)
+    if len(chat_list)>1:
+        if chat_list[len(chat_list)-1]['type'] == "assistant":
+            message = chat_list[len(chat_list)-2]['content']
+            VL_CHAT_LIST.pop()
+            VL_CHAT_LIST.pop()
+            chat_history.pop()
+            chat_history.pop()
+        elif chat_list[len(chat_list)-1]['type'] == "user":
+            message = chat_list[len(chat_list)-1]['content']
+            VL_CHAT_LIST.pop()
+            chat_history.pop()
+    return message,chat_history
+# 清除
+def vl_clear():
+    VL_CHAT_LIST.clear()
+    return None,"",[]
+# 返回问题答案
+def vl_submit(history_flag,chinese_flag,chat_history):
+    if len(VL_CHAT_LIST)>1:
+        messages = get_vl_message(history_flag,chinese_flag)
+        response = ollama.chat(
+            model = "llava:7b-v1.6",
+            messages = messages
+        )
+        result = response["message"]["content"]
+        output = {
+            "type":"assistant",
+            "content":result
+        }
+        chat_history.append((None,result))
+        VL_CHAT_LIST.append(output)
+    else:
+        gr.Warning('获取结果异常')
+    return chat_history
+def get_vl_message(history_flag,chinese_flag):
+    messages = []
+    if history_flag:
+        i=0
+        while i<len(VL_CHAT_LIST):
+            if VL_CHAT_LIST[i]['type']=="image" and VL_CHAT_LIST[i+1]['type']=="user":
+                image_path = VL_CHAT_LIST[i]["content"]
+                # 读取图像文件的二进制数据
+                with open(image_path, "rb") as image_file:
+                    image_data = image_file.read()
+                # 将二进制数据转换为 base64 编码的字符串
+                base64_string = base64.b64encode(image_data).decode("utf-8")
+                content = VL_CHAT_LIST[i+1]["content"]
+                chat_message = {
+                    'role': 'user', 
+                    'content': content,
+                    'images':[base64_string]
+                }
+                messages.append(chat_message)
+                i+=2
+            elif VL_CHAT_LIST[i]['type']=="assistant":
+                assistant_message = {
+                    "role":"assistant",
+                    "content":VL_CHAT_LIST[i]['content']
+                }
+                messages.append(assistant_message)
+                i+=1
+            elif VL_CHAT_LIST[i]['type']=="user":
+                user_message = {
+                    "role":"user",
+                    "content":VL_CHAT_LIST[i]['content']
+                }
+                messages.append(user_message)
+                i+=1
+            else:
+                i+=1
+    else:
+        if VL_CHAT_LIST[0]['type']=="image" and VL_CHAT_LIST[-1]['type']=="user":
+            image_path = VL_CHAT_LIST[0]["content"]
+            # 读取图像文件的二进制数据
+            with open(image_path, "rb") as image_file:
+                image_data = image_file.read()
+            # 将二进制数据转换为 base64 编码的字符串
+            base64_string = base64.b64encode(image_data).decode("utf-8")
+            content = VL_CHAT_LIST[-1]["content"]
+            chat_message = {
+                'role': 'user', 
+                'content': content,
+                'images':[base64_string]
+            }
+            messages.append(chat_message)
+    if chinese_flag:
+        system_message = {
+            'role': 'system', 
+            'content': 'You are a Helpal Assistant.Please answer the question in Chinese.请用中文回答'
+        }
+        messages.insert(0,system_message)
+    return messages
 with gr.Blocks(title="Ollama WebUI", fill_height=True) as demo:
     with gr.Tab("聊天"):
         with gr.Row():
@@ -94,7 +221,7 @@ with gr.Blocks(title="Ollama WebUI", fill_height=True) as demo:
                 prompt_model_info = gr.Dropdown(model_names, value="", allow_custom_value=True, label="模型选择")
                 prompt_info = gr.Dropdown(choices=PROMPT_LIST,value=PROMPT_LIST[0],label="智能体选择",interactive=True)
             with gr.Column(scale=4):
-                prompt_chat_bot = gr.Chatbot(height=540,render=False)
+                prompt_chat_bot = gr.Chatbot(height=600,render=False)
                 prompt_text_box = gr.Textbox(scale=4,render=False)
                 gr.ChatInterface(
                     fn=ollama_prompt,
@@ -107,5 +234,26 @@ with gr.Blocks(title="Ollama WebUI", fill_height=True) as demo:
                     clear_btn="🗑️ 清除",
                     fill_height=True
                 )
+    with gr.Tab("视觉助手"):
+        with gr.Row():
+            with gr.Column(scale=1):
+                history_flag = gr.Checkbox(label="启用上下文")
+                chinese_flag = gr.Checkbox(value=True,label="强制中文")
+                image = gr.Image(type="filepath")
+            with gr.Column(scale=4):
+                chat_bot = gr.Chatbot(height=600)
+                with gr.Row():
+                    retry_btn = gr.Button("🔄 重试")
+                    undo_btn = gr.Button("↩️ 撤消")
+                    clear_btn = gr.Button("🗑️ 清除")
+                with gr.Row():
+                    message = gr.Textbox(show_label=False,container=False,scale=5)
+                    submit_btn = gr.Button("提交",variant="primary",scale=1)
+        image.upload(fn=vl_image_upload,inputs=[image,chat_bot],outputs=[image,chat_bot])
+        submit_btn.click(fn=vl_submit_message,inputs=[message,chat_bot],outputs=[message,chat_bot]).then(fn=vl_submit,inputs=[history_flag,chinese_flag,chat_bot],outputs=[chat_bot])
+        retry_btn.click(fn=vl_retry,inputs=[chat_bot],outputs=[chat_bot]).then(fn=vl_submit,inputs=[history_flag,chinese_flag,chat_bot],outputs=[chat_bot])
+        undo_btn.click(fn=vl_undo,inputs=[chat_bot],outputs=[message,chat_bot])
+        clear_btn.click(fn=vl_clear,inputs=[],outputs=[image,message,chat_bot])
+    demo.load(fn=init)
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(share=False)
